e90e7944d268ffcbe20d382b08f0c71da6bc7a5d
cksek <chonkits@gmail.com>
2021-04-06 14:37:45 +0800

    CKSEK add in changes from frontend-demo-v2

--- src/main/java/com/oc/controller/AppController.java
+++ src/main/java/com/oc/controller/AppController.java
@@ -3,6 +3,10 @@
  */
 package com.oc.controller;
 
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.net.ssl.SSLContext;
 import javax.servlet.http.HttpServletRequest;
 
@@ -11,9 +15,12 @@ import org.apache.http.conn.ssl.NoopHostnameVerifier;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.ssl.SSLContextBuilder;
+import org.json.JSONObject;
 import org.keycloak.KeycloakPrincipal;
 import org.keycloak.KeycloakSecurityContext;
+import org.keycloak.TokenVerifier;
 import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
+import org.keycloak.representations.JsonWebToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
@@ -24,6 +31,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
 
@@ -41,8 +50,16 @@ public class AppController {
 
 	@Value("${trust.store.password}")
 	private String trustStorePassword;
+
+	@Value("${keycloak.realm}")
+	private String configuredRealm;
+
+	@Value("${keycloak.auth-server-url}")
+	private String configuredAuthServer;
 	
-	private final String rest_uri = "";
+	private String configuredIssuer;
+	
+	private final String rest_uri = "https://ec2-52-221-234-166.ap-southeast-1.compute.amazonaws.com:8443/octest-frontend-api";
 
 	@Autowired
 	private HttpServletRequest request;
@@ -101,10 +118,12 @@ public class AppController {
 		try {
 			
 			KeyCloakUserInfo usr_info = configModel(model);
+			String[] retVal = checkTicketPermission(getCurrentSecurityContext(), "user-author-sys-support").split("\\|");
 			
 			RestTemplate restful = restTemplate();
 			HttpHeaders headers = new HttpHeaders();
-			headers.add("Authorization", "Bearer " + usr_info.getAccessToken());
+			//headers.add("Authorization", "Bearer " + usr_info.getAccessToken());
+			headers.add("Authorization", "Bearer " + retVal[0]);
 			
 			HttpEntity<String> entity = new HttpEntity<>("body", headers);
 			ResponseEntity<String> resp = restful.exchange(rest_dest, HttpMethod.GET, entity, String.class); 
@@ -138,7 +157,8 @@ public class AppController {
 			ResponseEntity<String> resp = restful.exchange(rest_dest, HttpMethod.GET, entity, String.class); 
 
 			configPageModel(model, "page", resp.getBody());
-
+			configPageModel(model, "token", usr_info.getAccessToken());
+			
 		} catch (Exception e) {
 			System.err.println(METHOD_NM + " Exception: [" + e.getMessage() + "].");
 			configPageModel(model, "error", e.getMessage());
@@ -156,5 +176,64 @@ public class AppController {
 
 		return new RestTemplate(factory);
 	}
+	
+	private String checkTicketPermission(KeycloakSecurityContext sc, String targeted_aud) throws Exception {
+
+		String METHOD_NM = "checkTicketPermission()";
+		configuredIssuer = configuredAuthServer + "/realms/" + configuredRealm;
+
+		RestTemplate restful = restTemplate();
+		HttpHeaders headers = new HttpHeaders();
+		HttpEntity<MultiValueMap<String, String>> entity;
+		ResponseEntity<String> resp;
+		JSONObject obj;
+		
+		//Requesting Permission Ticket from KeyCloak
+		headers.add("Authorization", "Bearer " + sc.getTokenString());
+		headers.add("Content-Type", "application/x-www-form-urlencoded");
+
+		MultiValueMap<String, String> map = new LinkedMultiValueMap<String, String>();
+		map.add("grant_type", "urn:ietf:params:oauth:grant-type:uma-ticket");
+		map.add("audience", targeted_aud);
+
+		entity = new HttpEntity<MultiValueMap<String, String>>(map, headers);
+		resp = restful.exchange(configuredIssuer + "/protocol/openid-connect/token", HttpMethod.POST, entity, String.class);
+
+		System.out.println(METHOD_NM + "Response from Key Cloak: [" + resp.getBody() + "].");
+		
+		// Forming JSON Object with response from key cloak
+		obj = new JSONObject(resp.getBody());
+
+		// Form JsonWebToken based on permission ticket (access token)
+		JsonWebToken jwt = TokenVerifier.create(obj.get("access_token").toString(), JsonWebToken.class).getToken();
+		
+		/**
+		 * Developer may verify permission ticket by using following options
+		 * 1) Manual check for permission granted to users (based on resources)
+		 * OR
+		 * 2) Manual check for resources & roles granted to users
+		 * This POC having 2 method of checking for developer references.
+		 */
+		// Method 1:
+		Map<?,?> auth = (LinkedHashMap<?,?>)jwt.getOtherClaims().get("authorization");
+		List<?> auth_permission = (List<?>) auth.get("permissions");
+
+		for (int i = 0 ; i < auth_permission.size() ; i++) {
+			System.out.println(METHOD_NM + "Count [" + (i+1) + "]-"+ auth_permission.get(i));
+			//TODO teams can cross check with the resource value and its scopes to determine the access
+		}
+
+		// Method 2:
+		Map<?,?> auth_resource = (LinkedHashMap<?,?>)jwt.getOtherClaims().get("resource_access");
+		System.out.println(METHOD_NM + "Resource Access Class [" + auth_resource.keySet()+ "].");
+		
+		for (int i = 0 ; i < auth_resource.size() ; i++) {
+			System.out.println(METHOD_NM + "Count [" + (i+1) + "]-"+ auth_resource.get(targeted_aud)); //TODO to be define based on the user configuration
+			//TODO teams can cross check with the roles assigned to users to determine if it is accessible.
+		}
+		
+		return obj.get("access_token").toString() + "|" + auth_permission.toString() + "|" + auth_resource.get(targeted_aud);
+	}
+
 
 }
